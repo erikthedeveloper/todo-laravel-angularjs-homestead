1777608e29643bab243fe4b5a8a2ad591c4b1e25
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-02 05:31:16 -0600

    Adds migration create_users_table
    New table: users
    - `$table->increments('id');`
    - `$table->string('email');`
    - `$table->string('password');`
    - `$table->string('first_name');`
    - `$table->string('last_name');`
    - `$table->timestamps();`
    
    User model - Updates auto-docs via ide-helper

--- TheApp/LaravelApi/app/models/User.php
+++ TheApp/LaravelApi/app/models/User.php
@@ -5,6 +5,24 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
+/**
+ * User
+ *
+ * @property integer $id
+ * @property string $email
+ * @property string $password
+ * @property string $first_name
+ * @property string $last_name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereFirstName($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereLastName($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereUpdatedAt($value)
+ */
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
