1146f448e7365c7c3fc1c45a8e5d310621fb83dd
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-05 05:15:13 -0600

    Changes testing database to in memory sqlite

--- TheApp/LaravelApi/app/config/testing/database.php
+++ TheApp/LaravelApi/app/config/testing/database.php
@@ -2,46 +2,14 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Database Connections
-	|--------------------------------------------------------------------------
-	|
-	| Here are each of the database connections setup for your application.
-	| Of course, examples of configuring each database platform that is
-	| supported by Laravel is shown below to make development simple.
-	|
-	|
-	| All database work in Laravel is done through the PHP PDO facilities
-	| so make sure you have the driver for your particular database of
-	| choice installed on your machine before you begin development.
-	|
-	*/
+	'default' => 'sqlite',
 
-	'connections' => array(
-
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-	),
+        'connections' => array(
+            'sqlite' => array(
+                'driver'   => 'sqlite',
+                'database' => ':memory:',
+                'prefix'   => ''
+            ),
+        )
 
 );
