0fc9f028aafc4f6932ac7c2a3019964eac755f79
Erik Aybar <erikthedeveloper@gmail.com>
2014-07-15 05:53:30 -0600

    Behat/Routes - Behat running, hitting URLs (Guzzle), and asserting/passing (PHPUnit)
    - Feast your eyes
    Feature: Basic API Routes
        In order to interact with my application from the front end
        As a front end developer with no knowledge of API wizardry
        I need to have an awesomely clear API to work with
    
        Scenario: GET "/"
            When I send a GET request to "/"
            Then the response code should be 200
            And the JSON response should have a "message" containing "Hello World"

--- TheApp/LaravelApi/features/bootstrap/FeatureContext.php
+++ TheApp/LaravelApi/features/bootstrap/FeatureContext.php
@@ -7,18 +7,26 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+use GuzzleHttp\Client;
+
+/**
+ * Available auto-magically after composer install w/ phpunit/phpunit
+ */
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
 
 /**
  * Features context.
  */
 class FeatureContext extends BehatContext
 {
+
+    protected $client;
+
+    protected $response;
+
+    protected $base_url = 'http://api.my-todo-app.dev';
+
     /**
      * Initializes context.
      * Every scenario gets its own context object.
@@ -27,18 +35,36 @@ class FeatureContext extends BehatContext
      */
     public function __construct(array $parameters)
     {
-        // Initialize your context here
+        $client_params = [
+            'base_url' => $this->base_url
+        ];
+        $this->client = new Client($client_params);
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+    /**
+     * @When /^I send a ([A-Z]+) request to "([^"]*)"$/
+     */
+    public function iSendARequestTo($method, $uri)
+    {
+        $request        = $this->client->createRequest($method, $uri);
+        $this->response = $this->client->send($request);
+    }
+
+    /**
+     * @Then /^the response code should be (\d+)$/
+     */
+    public function theResponseCodeShouldBe($response_code)
+    {
+        assertEquals($response_code, $this->response->getStatusCode());
+    }
+
+    /**
+     * @Given /^the JSON response should have a "([^"]*)" containing "([^"]*)"$/
+     */
+    public function theJsonResponseShouldHaveAContaining($var_name, $var_contain_val)
+    {
+        $json_data = $this->response->json();
+        assertArrayHasKey($var_name, $json_data);
+        assertContains($var_contain_val, $json_data[$var_name]);
+    }
 }
--- TheApp/LaravelApi/features/first_behat_run.feature
+++
@@ -1,9 +0,0 @@
-Feature: Just some random behat feature...
-    In order to start using Behat
-    As a newbie BDD developer
-    I must figure it out first
-
-    Scenario: I need to be able to describe basic actions
-        Given I know what I want to do
-        When I decide to do that thing
-        Then That thing should happen
